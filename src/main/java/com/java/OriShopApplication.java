8bf22847deaad2e9cbf27e77535ad694c03c19ff
hoangan1610 <ngohoangan0@gmail.com>
2023-12-11 01:25:40 +0700

    Remove all files

---
+++ src/main/java/com/java/OriShopApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriShopApplication.class, args);
+	}
+
+}
